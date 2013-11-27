c2d547b67f92fce4468ea231b5fa1de781151298
Bob Nisco <BobNisco@gmail.com>
2013-11-27 12:58:14 -0500

    Added 2 test for initial moves for the black pieces

--- src/test/BoardTest.java
+++ src/test/BoardTest.java
@@ -37,4 +37,24 @@ public class BoardTest {
 		assertEquals("The board should have moved a knight", b.board[5][2], b.whiteKnight);
 		assertEquals("There should be no piece in the initial spot", b.board[7][1], 0);
 	}
+
+	@Test
+	public void testHandleMove02() throws Exception {
+		assertEquals("The board should have the pawn in the initial state", b.board[1][7], b.blackPawn);
+		Response r = new Response();
+		r.lastmove = "Ph7h5";
+		b.handleMove(r);
+		assertEquals("The board should have moved a Pawn into the new position", b.board[3][7], b.blackPawn);
+		assertEquals("There should be no piece in the initial spot", b.board[1][7], 0);
+	}
+
+	@Test
+	public void testHandleMove03() throws Exception {
+		assertEquals("The board should have the knight in the initial state", b.board[0][1], b.blackKnight);
+		Response r = new Response();
+		r.lastmove = "Nb8c6";
+		b.handleMove(r);
+		assertEquals("The board should have moved a knight", b.board[2][2], b.blackKnight);
+		assertEquals("There should be no piece in the initial spot", b.board[0][1], 0);
+	}
 }
