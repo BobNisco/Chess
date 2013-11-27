595a2382272e5c36455ea1b690b13e1c03f20c0d
Bob Nisco <BobNisco@gmail.com>
2013-11-26 23:06:11 -0500

    Initial commit

---
+++ src/Response.java
@@ -0,0 +1,31 @@
+/**
+ * A class representing the JSON response from the game server
+ * as outlined in the assignment PDF.
+ */
+public class Response {
+	public boolean ready;
+	public float secondsleft;
+	public int lastmovenumber;
+	public String lastmove;
+
+	public Response() {
+		this.ready = false;
+		this.secondsleft = -1;
+		this.lastmovenumber = -1;
+		this.lastmove = "";
+	}
+
+	public Response(boolean ready, float secondsleft, int lastmovenumber) {
+		this.ready = ready;
+		this.secondsleft = secondsleft;
+		this.lastmovenumber = lastmovenumber;
+		this.lastmove = null;
+	}
+
+	public Response(boolean ready, float secondsleft, int lastmovenumber, String lastmove) {
+		this.ready = ready;
+		this.secondsleft = secondsleft;
+		this.lastmovenumber = lastmovenumber;
+		this.lastmove = lastmove;
+	}
+}
