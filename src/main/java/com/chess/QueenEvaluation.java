7d385ae0a3a10a440106b9e2c63b62e08a57ed75
Mike Hunter <randomemail149@gmail.com>
2013-12-03 14:08:23 -0500

    KingEval

---
+++ src/main/java/com/chess/QueenEvaluation.java
@@ -0,0 +1,11 @@
+package main.java.com.chess;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MichaelHunter
+ * Date: 12/3/13
+ * Time: 2:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueenEvaluation {
+}
