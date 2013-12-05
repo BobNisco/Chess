96804ea52f811cae0f43cb6cbc8dd15d1fbbd355
Bob Nisco <BobNisco@gmail.com>
2013-12-05 15:11:50 -0500

    PlayChess plays against a random opponent

--- src/main/java/com/chess/Evaluator.java
+++ src/main/java/com/chess/Evaluator.java
@@ -2,8 +2,22 @@ package main.java.com.chess;
 
 public class Evaluator {
 
+	public static int kingBounty = 150;
+	public static int queenBounty = 90;
+	public static int rookBounty = 50;
+	public static int bishopBounty = 30;
+	public static int knightBounty = 30;
+	public static int pawnBounty = 10;
+
 	public static int evaluateBoard(Board b, int color) {
 		int sum = 0;
+
+		int opponentColor = 0;
+		if (color == Board.white) {
+			opponentColor = Board.black;
+		} else if (color == Board.black) {
+			opponentColor = Board.white;
+		}
 
 		try {
 			for (int rank = 0; rank < b.board.length; rank++) {
@@ -12,26 +26,32 @@ public class Evaluator {
 						case 1:
 							PawnEvaluation p = new PawnEvaluation(color);
 							sum += p.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 2:
 							KnightEvaluation n = new KnightEvaluation(color);
 							sum += n.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 3:
 							BishopEvaluation bishop = new BishopEvaluation(color);
 							sum += bishop.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 4:
 							RookEvaluation r = new RookEvaluation(color);
 							sum += r.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 5:
 							QueenEvaluation q = new QueenEvaluation(color);
 							sum += q.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 6:
 							KingEvaluation k = new KingEvaluation(color);
 							sum += k.b.board[rank][file];
+							bounty(b, sum, rank, file, opponentColor);
 							break;
 						case 0:
 							break;
@@ -45,5 +65,30 @@ public class Evaluator {
 		}
 
 		return sum;
+	}
+
+	private static void bounty(Board b, int sum, int rank, int file, int opponentColor) {
+		if (b.spaceHasOpponent(new Position(rank, file), opponentColor)) {
+			switch (Math.abs(b.board[rank][file])) {
+				case 1:
+					sum += pawnBounty;
+					break;
+				case 2:
+					sum += knightBounty;
+					break;
+				case 3:
+					sum += bishopBounty;
+					break;
+				case 4:
+					sum += rookBounty;
+					break;
+				case 5:
+					sum += queenBounty;
+					break;
+				case 6:
+					sum += kingBounty;
+					break;
+			}
+		}
 	}
 }
--- src/main/java/com/chess/PlayChess.java
+++ src/main/java/com/chess/PlayChess.java
@@ -1,34 +1,34 @@
 package main.java.com.chess;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class PlayChess {
 
 	public Board board;
 	public int color;
+	private Random randomGenerator;
 
 	public PlayChess(int color) {
 		this.board = new Board();
 		this.color = color;
+		this.randomGenerator = new Random();
 	}
 
 	public static void main(String[] args) {
 		PlayChess play = new PlayChess(Board.white);
-		System.out.println("START BOARD");
 		System.out.println(play.board);
-		Node nextNode = MiniMax.performMiniMax(play.board, play.color, 2);
-		System.out.println(nextNode.m);
-		play.board.handleMove(nextNode.m);
-		System.out.println(play.board);
-		play.board.handleMove(new Move(1, 1, 3, 1));
-		System.out.println(play.board);
-		nextNode = MiniMax.performMiniMax(play.board, play.color, 2);
-		System.out.println(nextNode.m);
-		play.board.handleMove(nextNode.m);
-		System.out.println(play.board);
-		play.board.handleMove(new Move(0, 1, 2, 2));
-		System.out.println(play.board);
-		nextNode = MiniMax.performMiniMax(play.board, play.color, 2);
-		System.out.println(nextNode.m);
-		play.board.handleMove(nextNode.m);
-		System.out.println(play.board);
+		int numberOfMoves = 12;
+		for (int i = 0; i < numberOfMoves; i++) {
+			Node nextNode = MiniMax.performMiniMax(play.board, play.color, 2);
+			play.board.handleMove(nextNode.m);
+			System.out.println(play.board + "\n\n");
+			ArrayList<Move> actions = GenerateSuccessors.allPossibleSuccessors(play.board, Board.black);
+			int index = play.randomGenerator.nextInt(actions.size());
+			play.board.handleMove(actions.get(index));
+			System.out.println(play.board);
+			System.out.println("----------------------------------");
+		}
 	}
 }
